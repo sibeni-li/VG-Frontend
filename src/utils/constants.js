89bbf27ee1f0f3fbdbd5a285ae66bd9a8d63baf8
sibeni-li <pro@sibeni.li>
2024-09-19 03:49:52 +0200

    Fetch url updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://vg-backend.onrender.com';
+const API_URL = 'https://vg01.sibeni.dev';
 export const API_ROUTES = {
   SIGN_UP: `${API_URL}/api/auth/signup`,
   SIGN_IN: `${API_URL}/api/auth/login`,
